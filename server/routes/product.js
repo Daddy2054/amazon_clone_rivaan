5ff73ff7fbf8a74ebf40ab3a9c28b8935e82c2ea
Jean <acalto2@gmail.com>
2023-06-29 21:38:47 +0200

    7.49

--- server/routes/product.js
+++ server/routes/product.js
@@ -22,14 +22,14 @@ productRouter.get('/api/products/search/:name', auth, async (req, res) => {
   }
 });
 // create a post request route to rate the product
-productRouter.post('/api/rate-product', auth, async (req, res) =>{
+productRouter.post('/api/rate-product', auth, async (req, res) => {
   try {
     const { id, rating } = req.body;
     let product = await Product.findById(id);
-     
-    for(let i=0; i<product.ratings.length; i++){
-      if(product.ratings[i].userId == req.user){
-        product.ratings.splice(i,1);
+
+    for (let i = 0; i < product.ratings.length; i++) {
+      if (product.ratings[i].userId == req.user) {
+        product.ratings.splice(i, 1);
         break;
       }
     }
@@ -43,7 +43,26 @@ productRouter.post('/api/rate-product', auth, async (req, res) =>{
 
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+});
+productRouter.get('/api/deal-of-day', auth, async (req, res) => {
+  try {
+    let products = await Product.find({});
+    products = products.sort((a, b) => {
+      let aSum = 0;
+      let bSum = 0;
 
+      for (let i = 0; i < a.ratings.length; i++) {
+        aSum += a.ratings[i].rating;
+      }
+      for (let i = 0; i < b.ratings.length; i++) {
+        bSum += b.ratings[i].rating;
+      }
+      return aSum < bSum ? 1 : -1;
+    });
+    res.json(products[0]);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
