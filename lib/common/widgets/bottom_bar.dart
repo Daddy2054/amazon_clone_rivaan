7d410da9eb254efb940ac53bac7a12bb334fec8d
Jean <acalto2@gmail.com>
2023-06-30 00:31:41 +0200

    8.51a

--- lib/common/widgets/bottom_bar.dart
+++ lib/common/widgets/bottom_bar.dart
@@ -1,5 +1,6 @@
 import 'package:amazon_clone_rivaan/constants/global_variables.dart';
 import 'package:amazon_clone_rivaan/features/account/screens/account_screen.dart';
+import 'package:amazon_clone_rivaan/features/cart/screens/cart_screen.dart';
 import 'package:amazon_clone_rivaan/features/home/screens/home_screen.dart';
 import 'package:amazon_clone_rivaan/providers/user_provider.dart';
 import 'package:flutter/material.dart';
@@ -21,9 +22,7 @@ class _BottomBarState extends State<BottomBar> {
   List<Widget> pages = [
     const HomeScreen(),
     const AccountScreen(),
-    const Center(
-      child: Text('Cart Page'),
-    ),
+    const CartScreen(),
   ];
   void updatePage(int page) {
     setState(() {
