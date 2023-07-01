2bf9e0ae79134e68b91336d707b15ee58e63c2d9
Jean <acalto2@gmail.com>
2023-07-01 23:26:38 +0200

    9.56

--- lib/features/account/widgets/orders.dart
+++ lib/features/account/widgets/orders.dart
@@ -1,5 +1,8 @@
+import 'package:amazon_clone_rivaan/common/widgets/loader.dart';
 import 'package:amazon_clone_rivaan/constants/global_variables.dart';
+import 'package:amazon_clone_rivaan/features/account/services/account_services.dart';
 import 'package:amazon_clone_rivaan/features/account/widgets/single_product.dart';
+import 'package:amazon_clone_rivaan/models/order.dart';
 import 'package:flutter/material.dart';
 
 class Orders extends StatefulWidget {
@@ -10,56 +13,78 @@ class Orders extends StatefulWidget {
 }
 
 class _OrdersState extends State<Orders> {
-  //temporary list
-  List list = [
-    'https://wikipedia.lurkmore.com/media/wikipedia/commons/thumb/3/35/Tux.svg/150px-Tux.svg.png',
-    'https://wikipedia.lurkmore.com/media/wikipedia/commons/thumb/3/35/Tux.svg/150px-Tux.svg.png',
-    'https://wikipedia.lurkmore.com/media/wikipedia/commons/thumb/3/35/Tux.svg/150px-Tux.svg.png',
-    'https://wikipedia.lurkmore.com/media/wikipedia/commons/thumb/3/35/Tux.svg/150px-Tux.svg.png',
-  ];
+  List<Order>? orders;
+  final AccountServices accountServices = AccountServices();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchOrders();
+  }
+
+  void fetchOrders() async {
+    orders = await accountServices.fetchMyOrders(context: context);
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              padding: const EdgeInsets.only(left: 15),
-              child: const Text(
-                'Your Orders',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
+    return orders == null
+        ? const Loader()
+        : Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(left: 15),
+                    child: const Text(
+                      'Your Orders',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(right: 15),
+                    child: Text(
+                      'See all',
+                      style: TextStyle(
+                        color: GlobalVariables.selectedNavBarColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              // display orders
+              Container(
+                height: 170,
+                padding: const EdgeInsets.only(
+                  left: 10,
+                  top: 20,
+                  right: 0,
+                ),
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: orders!.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        // Navigator.pushNamed(
+                        //   context,
+                        //   OrderDetailScreen.routeName,
+                        //   arguments: orders![index],
+                        // );
+                      },
+                      child: SingleProduct(
+                        image: orders![index].products[0].images[0],
+                      ),
+                    );
+                  },
                 ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(right: 15),
-              child: Text(
-                'See all',
-                style: TextStyle(
-                  color: GlobalVariables.selectedNavBarColor,
-                ),
-              ),
-            ),
-          ],
-        ),
-        //display orders
-        Container(
-          height: 170,
-          padding: const EdgeInsets.only(left: 10, top: 20, right: 0),
-          child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: list.length,
-              itemBuilder: (context, index) {
-                return SingleProduct(
-                  image: list[index],
-                );
-              }),
-        ),
-      ],
-    );
+            ],
+          );
   }
 }
